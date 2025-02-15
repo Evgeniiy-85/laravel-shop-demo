65177f702ac6cca51f4881d8c63bcfacb37c08a2
Evgeniiy-85 <evgeniy.aynulov@yandex.ru>
2025-02-15 17:06:44 +0400

    Добавил файлы

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('title')
+    Страница контактов
+@endsection
+
+@section('content')
+    <h1>Страница контактов</h1>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('title')
+    Главная страница
+@endsection
+
+@section('content')
+    <h1>!!</h1>
+@endsection
+
+
+@section('aside')
+    @parent
+    <p>!!!222s</p>
+@endsection
