e844cc8ed88e4f15c0310dd2fbb3e041b7a5de1f
Evgeniiy-85 <evgeniy.aynulov@yandex.ru>
2025-03-05 19:54:35 +0400

    Поправил ошибки

--- resources/views/admin/widgets/attachments/images.blade.php
+++ resources/views/admin/widgets/attachments/images.blade.php
@@ -1,5 +1,5 @@
 @if(!isset($without_container))
-    <div class="attachments ui-sortable" data-storage="{{ $storage }}" data-field_name="{{ $field }}" data-multiple="1" data-url="{{ route('api.admin.attachments.add') }}>
+    <div class="attachments ui-sortable" data-storage="{{ $storage }}" data-field_name="{{ $field }}" data-multiple="1" data-url="{{ route('api.admin.attachments.add') }}">
         @include('admin.widgets.attachments.partials.images')
     </div>
 @else
